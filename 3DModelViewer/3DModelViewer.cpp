dcb0ac5e2e05f1451392150f42aecf7250073609
Stephan <stephan.ihrke@gmail.com>
2020-09-12 18:32:08 +0200

    restructuring

---
+++ 3DModelViewer/3DModelViewer.cpp
@@ -0,0 +1,16 @@
+﻿// 3DModelViewer.cpp : Defines the entry point for the application.
+//
+
+#include "3DModelViewer.h"
+
+using namespace std;
+
+int main()
+{
+	if (!glfwInit())
+		return -1;
+
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
